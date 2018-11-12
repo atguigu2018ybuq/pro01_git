8b01998fcd8f8248367e5f7d8613031f018c509b
plus <plus@atguigu.com>
2018-11-12 18:23:57 +0800

    添加分支hot_fix

--- src/main/java/com/atguigu/git/Animal.java
+++ src/main/java/com/atguigu/git/Animal.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class Animal {
 	public static void main(String[] args) {
 		System.out.println("2222222222");
+		System.out.println("醉里挑灯看剑，梦回吹角连营，八百里分麾下炙，五十弦翻塞外声，沙场秋点兵。");
 	}
 }
